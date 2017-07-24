5f5c123a4fd2a516da4e1d57359f46de7d4bc234
hirasawa <ddt.jissyusei3@gmail.com>
2017-07-24 16:34:19 +0900

    ajax通信でJSONデータの取得するテストを行っています 10%

---
+++ 作業用/CPP/mysql_connect/DB_connect.h
@@ -0,0 +1,19 @@
+/* 概要 DBに接続するためのテスト用プログラム xammpにインストールされているmysqlが対象
+ *
+ * 作成日 2017年7月24日
+ *
+ * 作成者 平澤敬介
+ */
+
+#ifndef DB_CONNECT_H_
+#define DB_CONNECT_H_
+
+#include <mysql_connection.h>
+#include <mysql_driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
+
+#endif /* DB_CONNECT_H_ */
